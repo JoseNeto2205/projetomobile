6a80a2794599f6f3c62f5dcbdf75b15a67286d1d
Filipe Motta <femotta96@gmail.com>
2024-04-17 18:51:21 -0300

    feat(tab2): adiciona funções para ícones de senhas

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -4,8 +4,34 @@ import { SenhasService } from '../services/senhas.service';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
   constructor(public senhasService: SenhasService) {}
+
+  iconName(tipo: string): string {
+    switch (tipo) {
+      case 'SG':
+        return 'accessibility';
+      case 'SP':
+        return 'bandage';
+      case 'SE':
+        return 'document';
+      default:
+        return '';
+    }
+  }
+
+  iconColor(tipo: string): string {
+    switch (tipo) {
+      case 'SG':
+        return 'primary';
+      case 'SP':
+        return 'success';
+      case 'SE':
+        return 'warning';
+      default:
+        return '';
+    }
+  }
 }
