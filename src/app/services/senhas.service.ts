ccbc838c80335b5cd8f3632a6702f4ee007718a1
Filipe Motta <femotta96@gmail.com>
2024-04-17 18:46:17 -0300

    refactor: refatora lógica de chamada de senhas

--- src/app/services/senhas.service.ts
+++ src/app/services/senhas.service.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@angular/core';
 
-interface SenhasArray {
-  SG: string[];
-  SP: string[];
-  SE: string[];
+interface Senha {
+  tipo: string;
+  numero: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SenhasService {
   public inputNovaSenha: string = '';
-  public senhasArray: SenhasArray = {
-    SG:[],
-    SP:[],
-    SE:[]
-  }
+  public senhas: Senha[] = [];
 
   public senhasGeral: number = 0;
   public senhasPrior: number = 0;
@@ -23,71 +18,100 @@ export class SenhasService {
   public senhasTotal: number = 0;
 
   public senhaChamada: boolean = false;
+  public senhasChamadas: Senha[] = [];
 
-  somaGeral(){this.senhasGeral++;this.senhasTotal++;};
-  somaPrior(){this.senhasPrior++;this.senhasTotal++;};
-  somaExame(){this.senhasExame++;this.senhasTotal++;};
-
-  
-
-  novaSenha(tipoSenha: string = ''){
-    const data = new Date();
-    if (tipoSenha == 'SG'){
-      this.somaGeral();
-      this.inputNovaSenha =
-        data.getFullYear().toString().substring(2,4)+
-        (data.getMonth()+1).toString().padStart(2,'0')+
-        data.getDate().toString().padStart(2,'0')+
-        '-'+
-        tipoSenha +
-        (this.senhasArray['SG'].length + 1).toString().padStart(2,'0');
-      this.senhasArray.SG.push(this.inputNovaSenha);
-      } else if(tipoSenha == 'SP'){
-        this.somaPrior();
-        this.inputNovaSenha =
-          data.getFullYear().toString().substring(2,4)+
-          (data.getMonth()+1).toString().padStart(2,'0')+
-          data.getDate().toString().padStart(2,'0')+
-          '-'+
-          tipoSenha +
-          (this.senhasArray['SP'].length + 1).toString().padStart(2,'0');
-          this.senhasArray.SP.push(this.inputNovaSenha);
-      } else if(tipoSenha == 'SE'){
-        this.somaExame();
-        this.inputNovaSenha =
-          data.getFullYear().toString().substring(2,4)+
-          (data.getMonth()+1).toString().padStart(2,'0')+
-          data.getDate().toString().padStart(2,'0')+
-          '-'+
-          tipoSenha +
-          (this.senhasArray['SE'].length + 1).toString().padStart(2,'0');
-          this.senhasArray.SE.push(this.inputNovaSenha);
-      }
-      console.log(this.senhasArray);
-    }
-
-    chamarSenha() {
-      if (this.senhasArray.SP.length > 0) {
-        // Se houver senhas prioritárias, chama a próxima senha prioritária
-        this.inputNovaSenha = this.senhasArray.SP.pop() ?? '';
-      } else if (this.senhasArray.SE.length > 0) {
-        // Se não houver senhas prioritárias, mas houver senhas de exame, chama a próxima senha de exame
-        this.inputNovaSenha = this.senhasArray.SE.pop() ?? '';
-      } else if (this.senhasArray.SG.length > 0) {
-        // Se não houver senhas prioritárias nem de exame, chama a próxima senha geral
-        this.inputNovaSenha = this.senhasArray.SG.pop() ?? '';
-      } else {
-        // Se não houver mais senhas em nenhuma fila
-        this.inputNovaSenha = "Não há mais senhas";
-      }
-
-      // Defina senhaChamada como true após chamar a senha
-      this.senhaChamada = this.inputNovaSenha !== "Não há mais senhas";
-      return;
-    }
+  somaGeral() {
+    this.senhasGeral++;
+    this.senhasTotal++;
+  }
+  somaPrior() {
+    this.senhasPrior++;
+    this.senhasTotal++;
+  }
+  somaExame() {
+    this.senhasExame++;
+    this.senhasTotal++;
   }
 
-  
+  novaSenha(tipoSenha: string = '') {
+    const data = new Date();
+    let senha: Senha = { tipo: '', numero: '' };
+    if (tipoSenha == 'SG') {
+      this.somaGeral();
+      senha = {
+        tipo: 'SG',
+        numero:
+          data.getFullYear().toString().substring(2, 4) +
+          (data.getMonth() + 1).toString().padStart(2, '0') +
+          data.getDate().toString().padStart(2, '0') +
+          '-' +
+          tipoSenha +
+          this.senhasGeral.toString().padStart(2, '0'),
+      };
+    } else if (tipoSenha == 'SP') {
+      this.somaPrior();
+      senha = {
+        tipo: 'SP',
+        numero:
+          data.getFullYear().toString().substring(2, 4) +
+          (data.getMonth() + 1).toString().padStart(2, '0') +
+          data.getDate().toString().padStart(2, '0') +
+          '-' +
+          tipoSenha +
+          this.senhasPrior.toString().padStart(2, '0'),
+      };
+    } else if (tipoSenha == 'SE') {
+      this.somaExame();
+      senha = {
+        tipo: 'SE',
+        numero:
+          data.getFullYear().toString().substring(2, 4) +
+          (data.getMonth() + 1).toString().padStart(2, '0') +
+          data.getDate().toString().padStart(2, '0') +
+          '-' +
+          tipoSenha +
+          this.senhasExame.toString().padStart(2, '0'),
+      };
+    }
+    this.senhas.push(senha);
+    console.log(this.senhas);
+  }
 
-
-
+  chamarSenha() {
+    if (this.senhas.length > 0) {
+      for (let i = 0; i < this.senhas.length; i++) {
+        const senha = this.senhas[i];
+        if (senha.tipo == 'SP') {
+          this.inputNovaSenha = senha.numero;
+          this.senhas.splice(i, 1); // Remove a senha do array original
+          this.senhasChamadas.push(senha); // Adiciona a senha chamada ao array de senhas chamadas
+          this.senhaChamada = true;
+          return;
+        }
+      }
+      for (let i = 0; i < this.senhas.length; i++) {
+        const senha = this.senhas[i];
+        if (senha.tipo == 'SE') {
+          this.inputNovaSenha = senha.numero;
+          this.senhas.splice(i, 1); // Remove a senha do array original
+          this.senhasChamadas.push(senha); // Adiciona a senha chamada ao array de senhas chamadas
+          this.senhaChamada = true;
+          return;
+        }
+      }
+      if (this.senhas.length > 0) {
+        const senha = this.senhas[0]; // A próxima senha é uma senha geral
+        this.inputNovaSenha = senha.numero;
+        this.senhas.shift(); // Remove a senha do array original
+        this.senhasChamadas.push(senha); // Adiciona a senha chamada ao array de senhas chamadas
+        this.senhaChamada = true;
+      } else {
+        this.inputNovaSenha = 'Não há mais senhas';
+        this.senhaChamada = false;
+      }
+    } else {
+      this.inputNovaSenha = 'Não há mais senhas';
+      this.senhaChamada = false;
+    }
+  }
+}
