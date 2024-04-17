8c9ea810109279ca436b0b56912634f95aa20923
Filipe Motta <femotta96@gmail.com>
2024-04-17 18:47:19 -0300

    feat: adiciona métodos para ícone e cor de senha

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -4,10 +4,34 @@ import { SenhasService } from '../services/senhas.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  
-  constructor(public senhasService:SenhasService) {}
+  constructor(public senhasService: SenhasService) {}
 
+  iconName(tipo: string): string {
+    switch (tipo) {
+      case 'SG':
+        return 'accessibility';
+      case 'SP':
+        return 'bandage';
+      case 'SE':
+        return 'document';
+      default:
+        return '';
+    }
+  }
+
+  iconColor(tipo: string): string {
+    switch (tipo) {
+      case 'SG':
+        return 'primary';
+      case 'SP':
+        return 'success';
+      case 'SE':
+        return 'warning';
+      default:
+        return '';
+    }
+  }
 }
